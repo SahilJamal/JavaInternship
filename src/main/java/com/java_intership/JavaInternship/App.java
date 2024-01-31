11edc42988b49f130f640be35a0a73c399e81c23
Sahiljamal098 <sahiljamal098@gamil.com>
2024-01-31 08:51:26 +0530

    java_intern

---
+++ src/main/java/com/java_intership/JavaInternship/App.java
@@ -0,0 +1,71 @@
+package com.java_intership.JavaInternship;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class App 
+{
+	
+	private String cityName;
+	
+	private static final String API_KEY = "142cf43e95f6f0839c9e984cf674dd96"; 
+	
+    public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public static void main( String[] args )
+    {
+        
+        double temp = getWheactherData("Mumbai", API_KEY);
+        
+        if(temp > 20) {
+        	
+        	System.out.println("Deployment is done because temp is greater than 20");
+        	
+        	//Deployment logic
+        }else {
+        	System.out.println("Deployment is not done because temp is less than 20");
+        }
+        
+    }
+    
+    private static double getWheactherData(String city , String Api_code) {
+    	
+    	String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+Api_code+"&units=metric";
+    	
+    	HttpClient httpClient = HttpClient.newHttpClient();
+    	
+    	HttpRequest responce = HttpRequest.newBuilder()
+    			.uri(URI.create(apiUrl))
+    			.build();
+    	
+    	double temperature = 0.0;
+    	
+    	try {
+            
+            HttpResponse<String> httpResponse = httpClient.send(responce, HttpResponse.BodyHandlers.ofString());
+            
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(httpResponse.body());
+
+            JsonNode temperatureNode = jsonNode.path("main").path("temp");
+            temperature = temperatureNode.asDouble();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    	
+    	return temperature;
+    }
+}
+
